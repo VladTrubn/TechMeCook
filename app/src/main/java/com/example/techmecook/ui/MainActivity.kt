76902a7a4ac7471661ee4ea903883ca7ea5d8c56
Владислав Трубников <vladtrubn@gmail.com>
2021-05-16 20:05:10 +0700

    Добавлена функциональная кнопка logout Теперь можно очистить токен и заново ввести данные без необходимости перезапускать приложение.

--- app/src/main/java/com/example/techmecook/ui/MainActivity.kt
+++ app/src/main/java/com/example/techmecook/ui/MainActivity.kt
@@ -2,15 +2,15 @@ package com.example.techmecook.ui
 
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.techmecook.BuildConfig
 import com.example.techmecook.R
@@ -19,6 +19,7 @@ import com.example.techmecook.repositories.*
 import com.example.techmecook.model.*
 import com.example.techmecook.model.recipe.RandomRecipeCollection
 import com.example.techmecook.model.result.*
+import com.example.techmecook.util.SharedPref
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -39,7 +40,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_logout -> {
+                SharedPref.writeApiToken(applicationContext, "API_TOKEN")
+                Log.d("App", "token cleared")
+                val navController = findNavController(R.id.nav_host_fragment)
+                navController.navigateUp() // to clear previous navigation history
+                val navOption =
+                    NavOptions.Builder().setPopUpTo(R.id.recipeListFragment, true).build()
+                navController.navigate(R.id.loginFragment, null, navOption)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
